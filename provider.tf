ecf67c7e789a4bf7ff5769d187ca666d93842503
CVBalaji <133743538+CVBalaji@users.noreply.github.com>
2023-05-17 09:11:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIA5ZCONIVDBPACUMNY"
-  secret_key = "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"
+  access_key = "AKIA5ZCONIVDBPACUMNYNIS"
+  secret_key = "SjC1DhUKKHTHlOHvRTnKcsEnLiF2LzWZQOYvfT4shanu"
   region     = "us-east-1"
 }
   
